b55ee0949052d2054efc629d2d43c34cc40a88cf
zabronax <zabronax@gmail.com>
2025-04-04 11:00:45 +0200

    setup counter

--- main.js
+++ main.js
@@ -5,3 +5,29 @@ function handleClickA() {
 function handleClickB() {
   console.log("Button B clicked!")
 }
+
+function increment() {
+  console.log("Increment count")
+  let countElement = document.getElementById("count")
+
+  let previousValue = Number(countElement.textContent)
+  let newValue = previousValue + 1
+
+  countElement.textContent = newValue
+}
+
+function decrement() {
+  console.log("Decrement count")
+  let countElement = document.getElementById("count")
+  
+  let previousValue = Number(countElement.textContent)
+  let newValue = previousValue - 1
+
+  countElement.textContent = newValue
+}
+
+function reset() {
+  console.log("Resetting count")
+  let countElement = document.getElementById("count")
+  countElement.textContent = 0
+}
