d666a01791ebb628f5af8b7fa9c3af74273312c0
zabronax <zabronax@gmail.com>
2025-04-04 11:32:41 +0200

    add a function

--- main.js
+++ main.js
@@ -6,9 +6,14 @@ function handleClickB() {
   console.log("Button B clicked!")
 }
 
+function getCountElement() {
+  let countElement = document.getElementById("count")
+  return countElement
+}
+
 function increment() {
   console.log("Increment count")
-  let countElement = document.getElementById("count")
+  let countElement = getCountElement()
 
   let previousValue = Number(countElement.textContent)
   let newValue = previousValue + 1
@@ -18,14 +23,14 @@ function increment() {
 
 function decrement() {
   console.log("Decrement count")
-  let countElement = document.getElementById("count")
+  let countElement = getCountElement()
   
   let previousValue = Number(countElement.textContent)
   let isGreaterThanZero = previousValue > 0
 
   if (isGreaterThanZero) {
-  let newValue = previousValue - 1
-  countElement.textContent = newValue
+    let newValue = previousValue - 1
+    countElement.textContent = newValue
   } else {
     console.log("Number is 0")
   }
@@ -33,6 +38,6 @@ function decrement() {
 
 function reset() {
   console.log("Resetting count")
-  let countElement = document.getElementById("count")
+  let countElement = getCountElement()
   countElement.textContent = 0
 }
